bdb69295edfdf516fb677f060609139cb284ec87
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-25 04:06:55 -0700

    Update FBIModel.java

--- src/main/java/com/sparkshard/chaos/entity/FBIModel.java
+++ src/main/java/com/sparkshard/chaos/entity/FBIModel.java
@@ -1,19 +1,22 @@
+package com.sparkshard.chaos.entity;
+
+import com.sparkshard.chaos.ChaosMod;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib.model.GeoModel;
+
 public class FBIModel extends GeoModel<EntityFBI> {
     @Override
     public Identifier getModelResource(EntityFBI animatable) {
-        // Points to assets/chaos/geo/fbi_agent.geo.json
-        return new Identifier("chaos", "geo/fbi_agent.geo.json");
+        return new Identifier(ChaosMod.MODID, "geo/fbi_agent.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(EntityFBI animatable) {
-        // Points to assets/chaos/textures/entity/fbi_agent.png
-        return new Identifier("chaos", "textures/entity/fbi_agent.png");
+        return new Identifier(ChaosMod.MODID, "textures/entity/fbi_agent.png");
     }
 
     @Override
     public Identifier getAnimationResource(EntityFBI animatable) {
-        // Points to assets/chaos/animations/fbi_agent.animation.json
-        return new Identifier("chaos", "animations/fbi_agent.animation.json");
+        return new Identifier(ChaosMod.MODID, "animations/fbi_agent.animation.json");
     }
 }
