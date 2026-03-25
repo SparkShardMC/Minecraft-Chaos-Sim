498b3af21a27efbb6197884f252c937f1e126f06
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-25 04:08:04 -0700

    Update ChaosMod.java

--- src/main/java/com/sparkshard/chaos/ChaosMod.java
+++ src/main/java/com/sparkshard/chaos/ChaosMod.java
@@ -1,34 +1,23 @@
 package com.sparkshard.chaos;
 
 import com.sparkshard.chaos.commands.DisasterCommand;
-import com.sparkshard.chaos.commands.TPAICommand;
 import com.sparkshard.chaos.entity.EntityRegistry;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
-@Mod(ChaosMod.MODID)
-public class ChaosMod {
+public class ChaosMod implements ModInitializer {
     public static final String MODID = "chaos";
 
-    public ChaosMod(IEventBus modEventBus) {
-        // Registers the FBI/Mafia entities to the game
-        EntityRegistry.ENTITIES.register(modEventBus);
+    @Override
+    public void onInitialize() {
+        // Register Entities
+        EntityRegistry.register();
 
-        // Registers this class to the NeoForge event bus so it listens for commands
-        NeoForge.EVENT_BUS.register(this);
-        
-        System.out.println("--- SPARKSHARD CHAOS MOD: INITIALIZED ---");
-    }
+        // Register Commands
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            DisasterCommand.register(dispatcher);
+        });
 
-    @SubscribeEvent
-    public void onRegisterCommands(RegisterCommandsEvent event) {
-        // Links your command files to the game's / terminal
-        DisasterCommand.register(event.getDispatcher());
-        TPAICommand.register(event.getDispatcher());
-        
-        System.out.println("--- CHAOS COMMANDS: LOADED ---");
+        System.out.println("--- SPARKSHARD CHAOS MOD: INITIALIZED (FABRIC) ---");
     }
 }
