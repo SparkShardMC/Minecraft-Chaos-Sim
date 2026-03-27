6514bbc0de20d5986cd9b450ef48d615f139edb5
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-27 12:00:44 -0700

    Update FBIRenderer.java

--- src/main/java/com/sparkshard/chaos/entity/FBIRenderer.java
+++ src/main/java/com/sparkshard/chaos/entity/FBIRenderer.java
@@ -1,5 +1,11 @@
+package com.sparkshard.chaos.entity;
+
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+
 public class FBIRenderer extends GeoEntityRenderer<EntityFBI> {
     public FBIRenderer(EntityRendererFactory.Context renderManager) {
         super(renderManager, new FBIModel());
+        this.shadowRadius = 0.5f; // Adds a shadow under the FBI agent
     }
 }
